34c5e3b855e7772cab8c209564df75db576de567
zjd <tourbillon1994@outlook.com>
2017-07-05 21:47:39 +0800

    tijiaoshuoyou

---
+++ one/test2.java
@@ -0,0 +1,28 @@
+package one;
+
+public class test2 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void swap(char[] str , int one , int two) {
+		char temp = str[one];
+		str[one] = str[two];
+		str[two] = temp;
+	}
+	boolean is_swap(char[] str , int begin , int k) { // еп╤о
+		int i ;
+		for (i = begin ; i < k ; i ++) {
+			if (str[i] == str[k]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private static void permute(char[] str , int low , int high){
+		if (low == high) {
+			
+		}
+	}
+}
