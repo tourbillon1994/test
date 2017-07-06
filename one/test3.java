3f85882bd0a5c10dc0d0cb8aa40e1a70d7adbba7
zjd <tourbillon1994@outlook.com>
2017-07-06 17:14:23 +0800

    最大子序列和

---
+++ one/test3.java
@@ -0,0 +1,22 @@
+package one;
+/*
+ * 最大子序列和
+*/
+public class test3 {
+	public static void main (String args[]){
+		System.out.println( maxSubSum4(new int[]{1,2,2,-2,-4,4}) );
+	}
+	
+	public static int maxSubSum4(int[] a) {
+		int maxSum = 0 , thisSum = 0;
+		for(int i = 0 ; i < a.length ; i ++){
+			thisSum += a[i];
+			if (thisSum > maxSum){
+				maxSum = thisSum;
+			} else if (thisSum < 0) {
+				thisSum = 0;
+			}
+		}
+		return maxSum;
+	}
+}
